eec8c5124232bea80994f0b60f747533ad846b11
XgLsuLzRMy <32542170+XgLsuLzRMy@users.noreply.github.com>
2017-12-03 11:23:18 +0100

    Modifications

--- src/appChat/MessageList.java
+++ src/appChat/MessageList.java
@@ -1,5 +1,13 @@
 package appChat;
 
+import java.util.LinkedList;
+
 public class MessageList {
+	
+	LinkedList<Message> list;
+	
+	public MessageList() {
+		this.list = new LinkedList<Message>();
+	}
 
 }
