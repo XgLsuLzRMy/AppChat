c9d8e59ebca3df4f050cc76424a2fe59b1000a91
XgLsuLzRMy <32542170+XgLsuLzRMy@users.noreply.github.com>
2017-12-21 22:28:50 +0100

    Ajout javadoc

--- src/appChat/rmi/UserConsoleDistante.java
+++ src/appChat/rmi/UserConsoleDistante.java
@@ -14,12 +14,17 @@ import appChat.UtilisateurInexistantException;
 import appChat.UtilisateurList;
 import appChat.ihm.FenetreLogin;
 
-
-
+/**
+ * userConsoleDistante est la classe qui est executee par l'utilisateur pour
+ * lancer le programme. La classe ouvre la fenetre d'acceuil. Elle va chercher
+ * le registre et instancier un UtilisateurServeurImpl quand l'utilisateur aura
+ * reussi sa connection avec le serveur
+ *
+ */
 public class UserConsoleDistante {
 
 	public static final int PORT_SERVEUR = 1099;
-	public  int port_utilisateur;
+	public int port_utilisateur;
 
 	public static AppRMIServeur appDistant;
 	private UtilisateurServeur utilisateurServeur;
@@ -32,38 +37,44 @@ public class UserConsoleDistante {
 		// this.registry = registry;
 		this.port_utilisateur = 1099;
 		boolean reussi = false;
-		while(!reussi){
+		while (!reussi) {
 			try {
-				//UserConsoleDistante.registry = LocateRegistry.getRegistry(PORT_UTILISATEUR);
+				// UserConsoleDistante.registry = LocateRegistry.getRegistry(PORT_UTILISATEUR);
 				UserConsoleDistante.registry = LocateRegistry.getRegistry(this.port_utilisateur);
 				reussi = true;
 			} catch (RemoteException e) {
 				this.port_utilisateur++;
-				//e.printStackTrace();
+				// e.printStackTrace();
 			}
 		}
-		System.out.println("port_utilisateur = "+this.port_utilisateur);
+		System.out.println("port_utilisateur = " + this.port_utilisateur);
 		this.fenetreLogin = new FenetreLogin(this);
 		this.fenetreLogin.pack();
 		this.fenetreLogin.setVisible(true);
 
 	}
 
+	/**
+	 * Permet de creer un compte sur le serveur si le nom n'est pas deja pris.
+	 * 
+	 * @param nom
+	 *            Le nom du compte a creer
+	 * @param mdp
+	 *            Le mot de passe associe au compte
+	 * @return true si le compte a ete cree, false sinon
+	 */
 	public boolean creerCompte(String nom, String mdp) {
-		System.out.println("Tentative de creation de compte " + nom + " " + mdp
-				+ "... \n");
+		System.out.println("Tentative de creation de compte " + nom + " " + mdp + "... \n");
 
 		String ipaddress = null;
 		try {
 			ipaddress = InetAddress.getLocalHost().toString();
-			ipaddress = ipaddress.substring(ipaddress.indexOf("/") + 1,
-					ipaddress.length());
+			ipaddress = ipaddress.substring(ipaddress.indexOf("/") + 1, ipaddress.length());
 			System.out.println("Adresse ip locale du client : " + ipaddress);
 
 			try {
 				if (UserConsoleDistante.appDistant.utilisateurDejaExistant(nom) == false) {
-					UserConsoleDistante.appDistant.ajouterUtilisateur(nom, mdp,
-							ipaddress, this.port_utilisateur);
+					UserConsoleDistante.appDistant.ajouterUtilisateur(nom, mdp, ipaddress, this.port_utilisateur);
 					System.out.println("Creation compte OK");
 					return true;
 				}
@@ -79,6 +90,15 @@ public class UserConsoleDistante {
 
 	}
 
+	/**
+	 * Permet de connecte l'utilisateur au serveur, c'est a dire de recuperer ses
+	 * message et de pouvoir en envoyer.
+	 * 
+	 * @param nom
+	 *            Le nom de l'utilisateur qui se connecte
+	 * @param mdp
+	 *            Le mot de passe de connexion
+	 */
 	public void login(String nom, String mdp) {
 		System.out.println("Tentative de login " + nom + " " + mdp);
 		String ipaddress = null;
@@ -87,12 +107,11 @@ public class UserConsoleDistante {
 		} catch (UnknownHostException e1) {
 			e1.printStackTrace();
 		}
-		ipaddress = ipaddress.substring(ipaddress.indexOf("/") + 1,
-				ipaddress.length());
+		ipaddress = ipaddress.substring(ipaddress.indexOf("/") + 1, ipaddress.length());
 		System.out.println("Adresse ip locale du client : " + ipaddress);
 
 		Utilisateur u = null;
-		//Utilisateur u = new Utilisateur(nom, ipaddress);
+		// Utilisateur u = new Utilisateur(nom, ipaddress);
 
 		try {
 			if (UserConsoleDistante.appDistant.utilisateurDejaExistant(nom)) {
@@ -101,18 +120,13 @@ public class UserConsoleDistante {
 				System.out.println("2");
 
 				if (u != null) {
-					// this.fenetreLogin.dispatchEvent(new
-					// WindowEvent(this.fenetreLogin,
-					// WindowEvent.WINDOW_CLOSING));
-					// this.fenetreLogin.setVisible(false);
 					this.fenetreLogin.dispose();
 
 					try {
 
 						System.out.println("new utilisateurServeurImpl");
 
-						this.utilisateurServeur = new UtilisateurServeurImpl(u,
-								this);
+						this.utilisateurServeur = new UtilisateurServeurImpl(u, this);
 
 						// System.setProperty("java.rmi.server.hostname",this.utilisateurServeur.getUtilisateur().getIPAddress());
 						System.out.println("On rebind");
@@ -122,9 +136,7 @@ public class UserConsoleDistante {
 						e.printStackTrace();
 					}
 
-
 				}
-
 
 			} else {
 				// UserConsoleDistante.appDistant.ajouterUtilisateur(nom, mdp,
@@ -138,89 +150,28 @@ public class UserConsoleDistante {
 
 	}
 
-	public UtilisateurList getListeUtilisateursConnectes()
-			throws RemoteException {
+	/**
+	 * Permet de contacter le serveur pour recuperer la liste des utilisateur
+	 * connectes
+	 * 
+	 * @return La liste des utilisateurs connectes sous forme de UtilisateurList
+	 * @throws RemoteException
+	 */
+	public UtilisateurList getListeUtilisateursConnectes() throws RemoteException {
 		return UserConsoleDistante.appDistant.getListeUtilisateursConnectes();
 	}
 
-	/*
-	 * public void run() throws RemoteException { int choix = 1; Scanner lecture
-	 * = new Scanner(System.in);
-	 *
-	 * String str = ""; String nom = ""; int nb = 0;
-	 *
-	 * System.out.println("Messages recents : \n" +
-	 * this.getUtilisateur().getListMessagesRecents());
-	 *
-	 * while (choix != 0) { System.out.println("Que voulez-vous faire ?");
-	 * System.out.println("0 - Quitter");
-	 * System.out.println("1 - Publier un tweet");
-	 * System.out.println("2 - Voir les messages recents");
-	 * System.out.println("3 - Follow un utilisateur");
-	 * System.out.println("4 - Afficher les infos sur mon compte");
-	 * System.out.println("5 - Modifier le nombre max de messages récents");
-	 * System.out.println("6 - Afficher vos tweets"); str = lecture.nextLine();
-	 * while (str == "") { str = lecture.nextLine(); } choix =
-	 * Integer.parseInt(str);
-	 *
-	 * nom = ""; nb = 0; switch (choix) {
-	 *
-	 * case 0: appDistant.logout(this.utilisateurServeur.getUtilisateur());
-	 * break;
-	 *
-	 * case 1: System.out.print("Ecrire le contenu du tweet a publier : "); str
-	 * = lecture.nextLine(); while (str.isEmpty() == true) { str =
-	 * lecture.nextLine(); }
-	 *
-	 * this.envoyerMessage(str); break;
-	 *
-	 * case 2: System.out.println(this.utilisateurServeur.getUtilisateur().
-	 * getListMessagesRecents()); break;
-	 *
-	 * case 3: System.out.println("Donner le nom de l'utilisateur : "); nom =
-	 * lecture.nextLine(); while (nom.equals("")) {
-	 * System.out.println("Donner le nom de l'utilisateur : "); nom =
-	 * lecture.nextLine(); }
-	 *
-	 * this.follow(nom);
-	 *
-	 * break;
-	 *
-	 * case 4: System.out.println( "\nVous suivez " +
-	 * this.utilisateurServeur.getUtilisateur().getFollowCount() +
-	 * " utilisateurs"); System.out.println("Vous etes suivi par " +
-	 * this.utilisateurServeur.getUtilisateur().getFollowerCount() +
-	 * " utilisateurs"); System.out.println("Vous avez poste " +
-	 * this.utilisateurServeur.getUtilisateur().getListMessages().getNbMessage()
-	 * + " messages"); System.out.println("Vous avez fixe a " +
-	 * this.utilisateurServeur.getUtilisateur().getListMessagesRecents().
-	 * getNbMaxMessage() + " le nombre maximal de messages recents\n"); break;
-	 *
-	 * case 5:
-	 * System.out.println("Le nombre max de messages récents est actuellement de "
-	 * + this.utilisateurServeur.getUtilisateur().getListMessagesRecents().
-	 * getNbMaxMessage());
-	 * System.out.println("Donner le nouveau nombre max de messages récents : "
-	 * ); nb = Integer.parseInt(lecture.nextLine()); if (nb >= 0) {
-	 * this.utilisateurServeur.getUtilisateur().getListMessagesRecents().
-	 * setNbMaxMessage(nb);
-	 *
-	 * try {
-	 * UserConsoleDistante.appDistant.getUtilisateur(this.utilisateurServeur.
-	 * getUtilisateur().getNom()) .getListMessagesRecents().setNbMaxMessage(nb);
-	 * System.out.println("OK"); } catch (UtilisateurInexistantException e) {
-	 * e.printStackTrace(); } } else {
-	 * System.out.println("Nombre incorrect, on ne change rien"); } break; case
-	 * 6: System.out.println(this.utilisateurServeur.getUtilisateur().
-	 * getListMessagesUtilisateur()); break; default: System.out.println(choix +
-	 * " n'est pas un chox correct"); break; } } lecture.close(); }
+	/**
+	 * Permet de contacter le serveur pour follow un autre utilisateur identifie par
+	 * son nom donne en argument
+	 * 
+	 * @param nom
+	 *            Le nom de l'utilisateur que l'on souhaite follow
 	 */
-
 	public void follow(String nom) {
 		try {
 			// On ajoute un nouveau follower sur le serveur
-			UserConsoleDistante.appDistant.follow(this.utilisateurServeur
-					.getUtilisateur().getNom(), nom);
+			UserConsoleDistante.appDistant.follow(this.utilisateurServeur.getUtilisateur().getNom(), nom);
 		} catch (UtilisateurInexistantException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
@@ -228,20 +179,31 @@ public class UserConsoleDistante {
 		}
 	}
 
+	/**
+	 * Permet de contacter le serveur pour unfollow un autre utilisateur identifie
+	 * par son nom donne en argument
+	 * 
+	 * @param nom
+	 *            Le nom de l'utilisateur que l'on souhaite unfollow
+	 */
 	public void unfollow(String nom) {
 		try {
-			UserConsoleDistante.appDistant.unfollow(this.utilisateurServeur
-					.getUtilisateur().getNom(), nom);
+			UserConsoleDistante.appDistant.unfollow(this.utilisateurServeur.getUtilisateur().getNom(), nom);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Permet de contacter le serveur pour envoyer un message
+	 * 
+	 * @param str
+	 *            Le contenu du message
+	 */
 	public void envoyerMessage(String str) {
 		Message m;
 		try {
-			m = new Message(str, this.utilisateurServeur.getUtilisateur()
-					.getNom());
+			m = new Message(str, this.utilisateurServeur.getUtilisateur().getNom());
 			try {
 				UserConsoleDistante.appDistant.publieMessage(m);
 			} catch (RemoteException e) {
@@ -252,47 +214,110 @@ public class UserConsoleDistante {
 		}
 	}
 
+	/**
+	 * Permet de recuperer les informations sur l'utilisateur
+	 * 
+	 * @return L'utilisateur connecte
+	 * @throws RemoteException
+	 */
 	public Utilisateur getUtilisateur() throws RemoteException {
 		return this.utilisateurServeur.getUtilisateur();
 	}
 
+	/**
+	 * Permet de recuperer la liste des hashtags auxquels est abonne l'utilisateur
+	 * 
+	 * @return la liste des hashtags auxquels est abonne l'utilisateur sous forme de
+	 *         ArrayList
+	 * @throws RemoteException
+	 */
 	public ArrayList<String> getHashTagList() throws RemoteException {
 		if (this.utilisateurServeur == null) {
-			System.out
-					.println("Attention hashTagList vide car le serveur utilisateur n'est pas encore instancié");
+			System.out.println("Attention hashTagList vide car le serveur utilisateur n'est pas encore instancié");
 			return new ArrayList<String>();
 		} else {
 			return this.getUtilisateur().getHashTagList();
 		}
 	}
 
+	/**
+	 * Permet de recuperer la liste des hashtags recemment utilises par les
+	 * utilisateurs de AppChat
+	 * 
+	 * @return la liste des hashtags recemment utilises
+	 * @throws RemoteException
+	 */
 	public LinkedList<String> getHashTagsRecents() throws RemoteException {
 		return UserConsoleDistante.appDistant.getHashTagsRecents();
 	}
 
+	/**
+	 * Permet d'abonne l'utilisateur a un nouveau hashag
+	 * 
+	 * @param hashTag
+	 *            Le hashtag auquel on souhaite s'abonner
+	 */
 	public void ajouterHashTag(String hashTag) {
 		try {
-			UserConsoleDistante.appDistant.ajouterHashTag(this.getUtilisateur()
-					.getNom(), hashTag);
+			UserConsoleDistante.appDistant.ajouterHashTag(this.getUtilisateur().getNom(), hashTag);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Permet de desabonne l'utilisateur a un hashag
+	 * 
+	 * @param hashTag
+	 *            Le hashtag auquel on souhaite se desabonner
+	 */
 	public void retirerHashTag(String hashTag) {
 		try {
-			UserConsoleDistante.appDistant.retirerHashTag(this.getUtilisateur()
-					.getNom(), hashTag);
+			UserConsoleDistante.appDistant.retirerHashTag(this.getUtilisateur().getNom(), hashTag);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public ArrayList<Utilisateur> chercherUtilisateur(String text)
-			throws RemoteException {
+	/**
+	 * Permet de contacter le serveur pour chercher un autre utilisateur a partir de
+	 * son nom.
+	 * 
+	 * @param text
+	 *            Le nom de l'utilisateur que l'on cherche. text n'est pas forcement
+	 *            egale au nom de l'utilisateur : voir rechercherUtilisateur de
+	 *            AppChat
+	 * @return La liste des utilisateurs correspondant a la recherche
+	 * @throws RemoteException
+	 */
+	public ArrayList<Utilisateur> chercherUtilisateur(String text) throws RemoteException {
 		return UserConsoleDistante.appDistant.chercherUtilisateur(text);
 	}
 
+	/**
+	 * Cherche sur le serveur l'utilisateur possedant le nom donne en argument
+	 * 
+	 * @param nom
+	 *            Le nom de l'utilisateur que l'on cherche
+	 * @return L'utilisateur que l'on cherche
+	 */
+	public Utilisateur getUtilisateur(String nom) {
+		Utilisateur res = null;
+		try {
+			res = UserConsoleDistante.appDistant.getUtilisateur(nom);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (UtilisateurInexistantException e) {
+		}
+		return res;
+	}
+
+	/**
+	 * La fonction qui est execute par l'utilisateur pour lander le programme
+	 * 
+	 * @param args
+	 *            l'adresse IP du serveur
+	 */
 	public static void main(String[] args) {
 		Registry registry = null;
 		AppRMIServeur a = null;
@@ -300,10 +325,10 @@ public class UserConsoleDistante {
 		System.out.print("On cherche le registre distant... ");
 		try {
 			if (args.length > 0) {
-				//registry = LocateRegistry.getRegistry(args[0], PORT_SERVEUR);
+				// registry = LocateRegistry.getRegistry(args[0], PORT_SERVEUR);
 				registry = LocateRegistry.getRegistry(args[0]);
 			} else {
-				//registry = LocateRegistry.getRegistry(PORT_SERVEUR);
+				// registry = LocateRegistry.getRegistry(PORT_SERVEUR);
 				registry = LocateRegistry.getRegistry();
 			}
 			System.out.println("OK");
@@ -318,21 +343,6 @@ public class UserConsoleDistante {
 		} catch (NotBoundException ex) {
 			ex.printStackTrace();
 		}
-	}
-
-	/**
-	 * Cherche sur le serveur l'utilisateur possedant le nom donne en argument
-	 * @param nom Le nom de l'utilisateur que l'on cherche
-	 * @return L'utilisateur que l'on cherche
-	 */
-	public Utilisateur getUtilisateur(String nom) {
-		Utilisateur res = null;
-		try {
-			res = UserConsoleDistante.appDistant.getUtilisateur(nom);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (UtilisateurInexistantException e) { }
-		return res;
 	}
 
 }
